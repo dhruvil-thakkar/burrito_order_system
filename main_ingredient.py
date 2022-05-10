da71b7860a30aa35cb994c247edcb5f55eb8e7a2
victor.cruz <victor.cruz@rackspace.com>
2022-05-10 13:16:53 -0500

    change selection as integer

--- main_ingredient.py
+++ main_ingredient.py
@@ -1,5 +1,7 @@
 #this is an example of the switch used
-print ("Please select one of the fillowing ingredients")
+
+
+print ("Please select one of the fillowing ingredients by typing the number")
 print ("1 - Ground beef")
 print ("2 - Carne asada (add $1.00)")
 print ("3 - Carne adovada (add $1.00)")
@@ -7,32 +9,42 @@ print ("4 - Scrambled eggs")
 print ("5 - Chicken")
 print ("6 - Sofritas")
 
+#Decalaration of the variables used
 FIRST_INGREDIENT_SELECTED = False
-FIRST_INGREDIENT_NAME = NULL
+FIRST_INGREDIENT_NAME = ""
 FIRST_SELECTION = 0
+
 TOTAL_PRICE = 5
 
-if FIRST_SELECTION = str(1):
+if int(FIRST_SELECTION) == 1:
     FIRST_INGREDIENT_NAME = "Ground beef"
     FIRST_INGREDIENT_SELECTED = True
     
-if FIRST_SELECTION = str(2):
+if int(FIRST_SELECTION) == 2:
     FIRST_INGREDIENT_NAME = "Carne asada"
     FIRST_INGREDIENT_SELECTED = True
     TOTAL_PRICE = TOTAL_PRICE + 1
-if FIRST_SELECTION = str(3):
+if int(FIRST_SELECTION) == 3:
     FIRST_INGREDIENT_NAME = "Carne adovada"
     FIRST_INGREDIENT_SELECTED = True
     TOTAL_PRICE = TOTAL_PRICE + 1
-if FIRST_SELECTION = str(4):
+if int(FIRST_SELECTION) == 4:
     FIRST_INGREDIENT_NAME = "Scrambled eggs"
     FIRST_INGREDIENT_SELECTED = True
     
-if FIRST_SELECTION = str(5):
+if int(FIRST_SELECTION) == 5:
     FIRST_INGREDIENT_NAME = "Chicken"
     FIRST_INGREDIENT_SELECTED = True
     
-if FIRST_SELECTION = str(6):
+if int(FIRST_SELECTION) == 6:
     FIRST_INGREDIENT_NAME = "Sofritas"
     FIRST_INGREDIENT_SELECTED = True
     
+
+def int_check (FIRST_SELECTION):
+    try:
+        int(FIRST_SELECTION)
+        it_is = True
+    except ValueError:
+        it_is = False
+
