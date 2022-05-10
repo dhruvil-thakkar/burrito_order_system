569332fd3e2105df7b71aff5e1401580b39200d0
Jason Clark <atxjdc@gmail.com>
2022-05-10 13:03:08 -0500

    intro changes

---
+++ fillings.py
@@ -0,0 +1,16 @@
+# fillings
+
+"""
+Extra beans (add $ 0.50)
+Chorizo (add $1.00)
+Onion
+Peppers
+Salsa
+Green chile salsa (add $0.25)
+Cheese
+Extra cheese (add $.050)
+Potatoes (add $1.00)
+"""
+
+# Testing connection.
+print("     Fillings sub-module succesfully connected!")
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import intro
-import main_ingredient
-import rice_beans
-import fillings
+import intro #Jason
+import main_ingredient #Victor
+import rice_beans #Dhruv
+import fillings #Jason
 import confirm
+
+#Test globals here.
+print("                                                   Testing variables...")
+print("                                                   " + intro.name)
