f116838029a8feccb27a721cd4749eb1ffa8a964
victor.cruz <victor.cruz@rackspace.com>
2022-05-10 12:45:36 -0500

    initial commit for Main_ingredient

---
+++ main_ingredient.py
@@ -0,0 +1,38 @@
+#this is an example of the switch used
+print ("Please select one of the fillowing ingredients")
+print ("1 - Ground beef")
+print ("2 - Carne asada (add $1.00)")
+print ("3 - Carne adovada (add $1.00)")
+print ("4 - Scrambled eggs")
+print ("5 - Chicken")
+print ("6 - Sofritas")
+
+FIRST_INGREDIENT_SELECTED = False
+FIRST_INGREDIENT_NAME = NULL
+FIRST_SELECTION = 0
+TOTAL_PRICE = 5
+
+if FIRST_SELECTION = str(1):
+    FIRST_INGREDIENT_NAME = "Ground beef"
+    FIRST_INGREDIENT_SELECTED = True
+    
+if FIRST_SELECTION = str(2):
+    FIRST_INGREDIENT_NAME = "Carne asada"
+    FIRST_INGREDIENT_SELECTED = True
+    TOTAL_PRICE = TOTAL_PRICE + 1
+if FIRST_SELECTION = str(3):
+    FIRST_INGREDIENT_NAME = "Carne adovada"
+    FIRST_INGREDIENT_SELECTED = True
+    TOTAL_PRICE = TOTAL_PRICE + 1
+if FIRST_SELECTION = str(4):
+    FIRST_INGREDIENT_NAME = "Scrambled eggs"
+    FIRST_INGREDIENT_SELECTED = True
+    
+if FIRST_SELECTION = str(5):
+    FIRST_INGREDIENT_NAME = "Chicken"
+    FIRST_INGREDIENT_SELECTED = True
+    
+if FIRST_SELECTION = str(6):
+    FIRST_INGREDIENT_NAME = "Sofritas"
+    FIRST_INGREDIENT_SELECTED = True
+    
