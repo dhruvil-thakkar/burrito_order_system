6beafc8db757f4dc12e045831e7b7b72f85e9de7
dhruvil-thakkar <dhruvnimitbhulku@gmail.com>
2022-05-10 12:27:00 -0500

    Deleted Jasons dir

--- jason/intro.py
+++
@@ -1,9 +0,0 @@
-# Burrito Program
-
-def get_name():
-    print('     Welcome to the burritto order taking machine!\n')
-    name = input("     What is your name? ")
-    print("     Welcome " + name + "!")
-    return name
-
-get_name()
