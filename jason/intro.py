14f889348f7a58641dfd4548bc558ead3601acbd
Jason Clark <atxjdc@gmail.com>
2022-05-10 12:19:07 -0500

    starting in main dir

---
+++ jason/intro.py
@@ -0,0 +1,9 @@
+# Burrito Program
+
+def get_name():
+    print('     Welcome to the burritto order taking machine!\n')
+    name = input("     What is your name? ")
+    print("     Welcome " + name + "!")
+    return name
+
+get_name()
